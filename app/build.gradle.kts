9bf4f331bac084875d720f0907df87597ce86046
Saif-Khawaldeh <80422866+Saif-Khawaldeh@users.noreply.github.com>
2026-03-15 21:00:21 +0000

    changed version Gradle version: 8.10.2,                 Gradle plugin version: 8.8.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.cs4084_group_8"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.cs4084_group_8"
